092b712315d74ee41291accf439a9332b680595d
Albertpcprinc1 <achm.comp@gmail.com>
2026-03-02 20:13:27 -0500

    segunda subida prueba

---
+++ src/main/java/com/ejemplo/libreria/Util/ValidaLIbros.java
@@ -0,0 +1,17 @@
+package com.ejemplo.libreria.Util;
+
+import org.springframework.stereotype.Component;
+
+import com.ejemplo.libreria.Model.Libros;
+
+@Component
+public class ValidaLIbros {
+    public Libros libro;
+
+    public boolean tituloValido(){
+        return libro.getTitulo() !=null && !Boolean.parseBoolean(libro.getTitulo());
+
+
+    }
+
+}
